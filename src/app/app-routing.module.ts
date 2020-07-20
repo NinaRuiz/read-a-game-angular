fc6fc240c01734740f7c3cd0c67d8ab39bbe27d8
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-20 18:53:57 +0200

    Add directory: call-of-cthulhu and move in it the call-of-cthulhu-page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
-import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu-page/call-of-cthulhu-page.component";
+import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu/call-of-cthulhu-page/call-of-cthulhu-page.component";
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { CallOfCthulhuPageComponent } from './pages/call-of-cthulhu-page/call-of-cthulhu-page.component';
+import { CallOfCthulhuPageComponent } from './pages/call-of-cthulhu/call-of-cthulhu-page/call-of-cthulhu-page.component';
 
 @NgModule({
   declarations: [
