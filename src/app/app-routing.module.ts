ebc92c8caf747c147f6d8d41a1a19294fa5b93f5
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-20 19:13:27 +0200

    Add route to create-character page and linked trow call-of-cthulhu-page button.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu/call-of-cthulhu-page/call-of-cthulhu-page.component";
+import {CreateCharacterFirstPageComponent} from "./pages/call-of-cthulhu/create-character-first-page/create-character-first-page.component";
 
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
-  {path: 'call-of-cthulhu', component: CallOfCthulhuPageComponent}
+  {path: 'call-of-cthulhu/create-character', component: CreateCharacterFirstPageComponent},
+  {path: 'call-of-cthulhu', component: CallOfCthulhuPageComponent},
 ];
 
 @NgModule({
