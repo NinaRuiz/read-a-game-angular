e28a3efd4fafb2e3df2d070f57b0dd4bf4978aae
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-20 18:47:55 +0200

    Add path to call-of-cthulhu-page and linked trow a home-page button.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
+import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu-page/call-of-cthulhu-page.component";
 
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'call-of-cthulhu', component: CallOfCthulhuPageComponent}
 ];
 
 @NgModule({
