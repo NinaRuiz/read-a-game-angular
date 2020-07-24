29df1deaa976bdfdb57331cf6270e489fd2590c7
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-24 16:22:41 +0200

    Add route to create-character/first-page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu/call-of-cthulhu-page/call-of-cthulhu-page.component";
 import {CharactersManagerPageComponent} from "./pages/call-of-cthulhu/characters-manager-page/characters-manager-page.component";
+import {FirstPageComponent} from "./pages/call-of-cthulhu/create-character/first-page/first-page.component";
 
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
+  {path: 'call-of-cthulhu/character-manager/create-character-1', component: FirstPageComponent},
   {path: 'call-of-cthulhu/character-manager', component: CharactersManagerPageComponent},
   {path: 'call-of-cthulhu', component: CallOfCthulhuPageComponent},
 ];
