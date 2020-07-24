6f652ed26eb9221d26d04eb06fe64f8b28f0c6ac
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-24 20:38:52 +0200

    Add route to create-character/second-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {HomePageComponent} from './pages/home-page/home-page.component';
 import {CallOfCthulhuPageComponent} from "./pages/call-of-cthulhu/call-of-cthulhu-page/call-of-cthulhu-page.component";
 import {CharactersManagerPageComponent} from "./pages/call-of-cthulhu/characters-manager-page/characters-manager-page.component";
 import {FirstPageComponent} from "./pages/call-of-cthulhu/create-character/first-page/first-page.component";
+import {SecondPageComponent} from "./pages/call-of-cthulhu/create-character/second-page/second-page.component";
 
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
+  {path: 'call-of-cthulhu/character-manager/create-character-2', component: SecondPageComponent},
   {path: 'call-of-cthulhu/character-manager/create-character-1', component: FirstPageComponent},
   {path: 'call-of-cthulhu/character-manager', component: CharactersManagerPageComponent},
   {path: 'call-of-cthulhu', component: CallOfCthulhuPageComponent},
