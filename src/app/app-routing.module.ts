cd7f95982ce9f546c6d078344215d9f3c24510e6
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-26 22:20:30 +0200

    Change input types and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
+import {RegisterPageComponent} from "./pages/register-page/register-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
+import {AboutThisPageComponent} from "./pages/about-this-page/about-this-page.component";
 
 const routes: Routes = [
+  {path: 'about-this', component: AboutThisPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
   {path: '', component: HomePageComponent},
-  { path: 'call-of-cthulhu', loadChildren: () => import('./call-of-cthulhu-module/call-of-cthulhu-module.module').then(m => m.CallOfCthulhuModuleModule) },
+  { path: 'call-of-cthulhu',
+    loadChildren: () => import('./call-of-cthulhu-module/call-of-cthulhu-module.module')
+      .then(m => m.CallOfCthulhuModuleModule) },
 ];
 
 @NgModule({
